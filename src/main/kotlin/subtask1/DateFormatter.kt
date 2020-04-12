b5d8376e473d1dce22dbb3377f77df256991ae43
Olga Chaiko <chaiko.olga@gmail.com>
2020-04-13 00:53:36 +0300

    add subtask1 and subtask2

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,24 @@
 package subtask1
 
+
+import java.time.DateTimeException
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        try {
+            val outputDate = "d MMMM, EEEE"
+            val currentDate = LocalDate.of(year.toInt(), month.toInt(), day.toInt())
+            val formatter = DateTimeFormatter.ofPattern(outputDate, Locale("RU"))
+            return formatter.format(currentDate)
+        } catch (e: DateTimeException) {
+            return "Такого дня не существует"
+        }
     }
 }
+
+
+
--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -2,8 +2,53 @@ package subtask2
 
 class TimeConverter {
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val hours = hour.toInt()
+        val minutes = minute.toInt()
+
+        val minutesArray = arrayOf(
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+            "twenty",
+            "twenty one",
+            "twenty two",
+            "twenty three",
+            "twenty four",
+            "twenty five",
+            "twenty six",
+            "twenty seven",
+            "twenty eight",
+            "twenty nine"
+        )
+        return when (minutes) {
+            0 -> "${minutesArray[hours]} o' clock"
+            1 -> "one minute past ${minutesArray[hours]}"
+            in 2..14 -> "${minutesArray[minutes]} minutes past ${minutesArray[hours]}"
+            15 -> "quarter past ${minutesArray[hours]}"
+            in 16..29 -> "${minutesArray[minutes]} minutes past ${minutesArray[hours]}"
+            30 -> "half past ${minutesArray[hours]}"
+            in 31..44 -> "${minutesArray[60 - minutes]} minutes to ${minutesArray[hours + 1]}"
+            45 -> "quarter to ${minutesArray[hours + 1]}"
+            in 46..59 -> "${minutesArray[60 - minutes]} minutes to ${minutesArray[hours + 1]}"
+            else -> ""
+        }
     }
 }
