64a7342e8f54790e921298e59c86bcd288b557e2
Olga Chaiko <chaiko.olga@gmail.com>
2020-04-13 18:31:12 +0300

    add subtask3

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -2,8 +2,30 @@ package subtask3
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val str = a.toUpperCase()
+        var i = 0
+        var j = 0
+        var k = 0
+        while (i < str.length) {
+            if (b[j] == str[i]) {
+                i++
+                k++
+                if (j < b.length) {
+                    j++
+                }
+            } else {
+                i++
+            }
+            if (k == b.length) {
+                break
+            }
+        }
+        return if (b.length == k) {
+            "YES"
+        } else {
+            "NO"
+        }
     }
 }
